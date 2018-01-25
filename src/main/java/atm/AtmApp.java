224bb091c1e0c6aa251eb14dfada65befdacfa86
Gilford <vincentgilford@gmail.com>
2018-01-25 16:52:52 -0500

    Balance feature added

--- src/main/java/atm/AtmApp.java
+++ src/main/java/atm/AtmApp.java
@@ -30,36 +30,43 @@ public class AtmApp {
 		
 		while(!optionEntered.equals("4")){	
 		
-				
-				if(optionEntered.equals("2")){
-					System.out.println("Please enter amount to withdraw?");
-					int withDrawAmount = input.nextInt(); 
-					myAccount.withDraw(withDrawAmount);
-					System.out.println("You have withdrawn " + withDrawAmount +" and your balance is now " + myAccount.getBalance());
-				}
-				
-				if(optionEntered.equals("1")){
-					System.out.println("Please enter amount to deposit?");
-					int moneyToDeposit = input.nextInt();
-					myAccount.depositMoney(moneyToDeposit);
-					System.out.println("You have deposited " + moneyToDeposit +" and your balance is now " + myAccount.getBalance());
-				}
-				
-				
-		
-				System.out.println("Choose an option.");
-				System.out.println("Press 1 to deposit funds.");
-				System.out.println("Press 2 to withdrawal funds.");
-				System.out.println("Press 3 to check balance.");
-				System.out.println("Press 4 to exit.");
-				optionEntered = input.next();
-				
-				
-				if(optionEntered.equals("4")) {
-					System.out.println("GoodBye");
-					System.exit(0);
-				}
-	
+			if (optionEntered.equals("1")) {
+				System.out.println("Please enter amount to deposit?");
+				int moneyToDeposit = input.nextInt();
+				myAccount.depositMoney(moneyToDeposit);
+				System.out.println(
+						"You have deposited " + moneyToDeposit + " and your balance is now " + myAccount.getBalance());
+			}
+
+			if (optionEntered.equals("2")) {
+				System.out.println("Please enter amount to withdraw?");
+				int withDrawAmount = input.nextInt();
+				myAccount.withDraw(withDrawAmount);
+				System.out.println(
+						"You have withdrawn " + withDrawAmount + " and your balance is now " + myAccount.getBalance());
+			}
+
+			
+			if(optionEntered.equals("3")) {
+				System.out.println("Your Balance is: " + myAccount.getBalance());	
+			}
+			
+			
+			
+			System.out.println("Choose an option.");
+			System.out.println("Press 1 to deposit funds.");
+			System.out.println("Press 2 to withdrawal funds.");
+			System.out.println("Press 3 to check balance.");
+			System.out.println("Press 4 to exit.");
+			optionEntered = input.next();
+
+			
+			if (optionEntered.equals("4")) {
+				System.out.println("GoodBye");
+				System.exit(0);
+			
+			
+			}
 		
 		}
 		
